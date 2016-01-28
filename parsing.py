de9ccd0dba4bdca4376d317f5bee6caba0571704
David Ricardo Jaramillo Baranda <jaradricc@gmail.com>
2016-01-28 11:30:49 -0600

    version 0.8
    Description:
    There was implemented a method that creates the B matrix  (tokens x tags) with the corresponding probabilities

--- parsing.py
+++ parsing.py
@@ -11,7 +11,8 @@
 # Counter(elem['tag'] for elem in jt)
 #
 # 5. Para eliminar las líneas en blanco, ejecutamos en bash : sed '/^$/d' corpus_formateado.txt > corpus_formateado2.txt
-
+import numpy
+import json
 
 def parse_document(jl, d):
     for i in jl:
@@ -31,24 +32,34 @@ def get_tags(d):
                 t.append(it)
     return t
 
-import json
-r = dict()
+def get_bmatrix(d,t):
+    bm = numpy.zeros((len(d),len(t)))  #Inicializamos una matriz de tamaño (#tokens, #tags)
+    for i, ii in zip(d.items(), bm):
+        tot = float(len(i[1]))   #Número total de veces que aparece el token i[0]
+        for tag in i[1]:
+            ii[t.index(tag)] += 1/tot            # ii es el renglón a editar de la matriz bm, el indice al que debemos sumar corresponde  la posicion en la lista de tags (t)
+    return bm
 
-arch = open('corpus_formateado2.txt', 'r')
-for line in arch:
-    if line != "":
-        jt =json.loads(json.dumps(eval(line)))
-        parse_document(jt,r)
-arch.close()
-# jt = json.loads(json.dumps([{"token":"Buda_Gautama_De_Wikipedia","tag":"NP"},{"token":",","tag":"Fc"},{"token":"la","tag":"DA"},{"token":"enciclopedia","tag":"NC"},{"token":"libre","tag":"AQ"},{"token":"Buda_Gautama_Buddha","tag":"NP"},{"token":"in","tag":"NC"},{"token":"Sarnath_Museum","tag":"NP"},{"token":"(","tag":"Fp"},{"token":"Dhammajak_Mutra","tag":"NP"},{"token":")","tag":"Fp"},{"token":".","tag":"Fp"},{"token":"jpg","tag":"NC"},{"token":"Estatua_de_el_Buda_Gautama","tag":"NP"},{"token":"de","tag":"SP"},{"token":"el","tag":"DA"},{"token":"siglo","tag":"NC"},{"token":"IV","tag":"NP"},{"token":"a","tag":"SP"},{"token":".","tag":"Fp"}],[{"token":"C.","tag":"NP"},{"token":"en","tag":"SP"},{"token":"la","tag":"DA"},{"token":"ciudad","tag":"NC"},{"token":"de","tag":"SP"},{"token":"Sarnath","tag":"NP"},{"token":",","tag":"Fc"},{"token":"distrito","tag":"NC"},{"token":"de","tag":"SP"},{"token":"Benar\u00e9s","tag":"NP"},{"token":",","tag":"Fc"},{"token":"estado","tag":"NC"},{"token":"de","tag":"SP"},{"token":"Uttar_Pradesh","tag":"NP"},{"token":",","tag":"Fc"},{"token":"India","tag":"NP"},{"token":".","tag":"Fp"}]))
-#print jt
-#
-# r= dict()
-# parse_document(jt, r)
 
-for key, values in r.items():
-    print key, values
-print '\n'
+def main():
+    r = dict()
 
-t = get_tags(r)
-print 'tags: ', t
+    arch = open('corpus_formateado2.txt', 'r')
+    for line in arch:
+        if line != "":
+            jt =json.loads(json.dumps(eval(line)))
+            parse_document(jt,r)
+    arch.close()
+
+
+    for key, values in r.items():
+        print key, values
+    print '\n'
+
+    t = get_tags(r)
+    print 'tags: ', t
+    pass
+
+
+if __name__ == "__main__":
+    main()
